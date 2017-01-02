62a3ae87d7d640c080e232b812620f6a97b89e30
Orion Buske <orion.buske@gmail.com>
2017-01-02 04:09:04 -0500

    Added request logging and recent request display

--- server/__init__.py
+++ server/__init__.py
@@ -2,18 +2,27 @@ import os
 
 import logging
 import json
+import flask
 
-from flask import after_this_request, g, jsonify, render_template, request
+from datetime import datetime
+from elasticsearch_dsl import Search
+from flask import after_this_request, jsonify, render_template, request as flask_request
 from flask_negotiate import consumes, produces
 from werkzeug.exceptions import BadRequest
 
-from mme_server.decorators import auth_token_required
+from mme_server.auth import auth_token_required, get_server_manager
 from mme_server.server import app, API_MIME_TYPE, get_backend
 from mme_server.models import MatchRequest, MatchResponse
 from mme_server.schemas import validate_request, validate_response, ValidationError
 
-from .compat import urlopen, Request
+from .compat import urlopen, HTTPError, Request
 
+VERSION = '0.1'
+USER_AGENT = 'mme-exchange-server/{}'.format(VERSION)
+ES_INDEX = 'exchange'
+ES_LOG_TYPE = 'request'
+
+logging.basicConfig(level='INFO')
 logger = logging.getLogger(__name__)
 
 # Default to development configuration
@@ -21,106 +30,146 @@ app_settings = os.getenv('APP_SETTINGS', 'config.dev.Config')
 app.config.from_object(app_settings)
 app.template_folder = os.path.join(os.path.dirname(os.path.abspath(__file__)), 'templates')
 
-def get_connected_servers():
-    db = get_backend()
-    response = db.servers.list()
-    servers = {}
+
+def get_outgoing_servers():
+    """Get a list of outgoing servers"""
+    servers = get_server_manager()
+    response = servers.list()
+    servers = []
     for server in response.get('rows', []):
         if server.get('direction') == 'out':
-            server_id = server['server_id']
-            servers[server_id] = server
+            servers.append(server)
 
     return servers
 
 
-def send_request(server, request_data, timeout):
-    base_url = server['base_url']
-    assert base_url.startswith('https://')
+def get_outgoing_server(server_id):
+    """Get a list of outgoing servers"""
+    servers = get_server_manager()
+    s = servers.index.search()
+    s = s.filter('term', server_id=server_id)
+    s = s.filter('term', direction='out')
+    results = s.execute()
 
-    url = '{}/match'.format(base_url)
+    if results.hits:
+        return results.hits[0]
+
+
+def get_recent_requests(n=10):
+    db = get_backend()
+    s = Search(using=db._db, index=ES_INDEX, doc_type=ES_LOG_TYPE)
+    s = s.sort('-created_at')
+    s = s[:n]
+    results = s.execute()
+    return results.hits
+
+
+def log_request(sender_id, raw_request, request, receiver_id, response_code, raw_response, response, request_at, response_time):
+    """Store the provided request/response for quality assurance
+
+    sender_id - the sending server id
+    request - a dictionary of the MME request
+    reciever_id - the receiving server id
+    response - a dictionary of the MME response
+    """
+    db = get_backend()
+    query_patient_id = request.get('patient', {}).get('id', '')
+    response_patient_ids = []
+    for result in response.get('results', []):
+        pid = result.get('patient', {}).get('id', '')
+        response_patient_ids.append(pid)
+
+    now = datetime.now().isoformat()
+    doc = {
+        'sender_id': sender_id,
+        'receiver_id': receiver_id,
+        'query_patient_id': query_patient_id,
+        'response_patient_ids': response_patient_ids,
+        'request': json.dumps(request),
+        'response': json.dumps(response),
+        'created_at': request_at,
+        'status': response_code,
+        'took': response_time,
+    }
+    try:
+        db._db.index(index=ES_INDEX, doc_type=ES_LOG_TYPE, body=doc)
+    except Exception as e:
+        logger.warning('Error logging request: {}'.format(e))
+
+
+def send_request(server_id, request, timeout):
+    """Send the request to the given server and return a tuple (status, response, elapsed_time)
+
+    Elapsed time is in decimal seconds
+    Raises TimeoutError if the request takes longer than timeout seconds
+    Status of 0 is returned if an error occured processing the request
+    """
     headers = {
-        'User-Agent': 'mme-server/0.2',
+        'User-Agent': USER_AGENT,
         'Content-Type': API_MIME_TYPE,
         'Accept': API_MIME_TYPE,
     }
+    code = 0
+    response = {}
+    elapsed_time = 0
 
-    auth_token = server.get('server_key')
+    # Serialize request
+    request_data = json.dumps(request).encode('utf-8')
+
+    server = get_outgoing_server(server_id)
+    if not server:
+        error = 'Unable to find server: {}'.format(server_id)
+        logger.error(error)
+        return (code, { 'message': error }, elapsed_time)
+
+    auth_token = server['server_key']
     if auth_token:
         headers['X-Auth-Token'] = auth_token
+        logger.info('Authenticating with: {}...'.format(auth_token[:4]))
 
-    print("Opening request to URL: " + url)
-    print("Sending request: " + request_data.decode())
-    req = Request(url, data=request_data, headers=headers)
-    handler = urlopen(req)
+    base_url = server['base_url']
+    assert base_url.startswith('https://')
+    match_url = '{}/match'.format(base_url)
 
-    print("Loading response")
-    response = handler.read().decode('utf-8')
-    response_json = json.loads(response)
-
-    logger.info("Validating response syntax")
+    logger.info("Opening request to URL: " + match_url)
+    logger.info("Sending request: " + request_data.decode())
     try:
-        validate_response(response_json)
-    except ValidationError as e:
-        # log to console and return response anyway
-        logger.error('Response does not conform to API specification:\n{}'.format(e))
+        sent_request_at = datetime.now()
+        req = Request(match_url, data=request_data, headers=headers)
+        try:
+            handler = urlopen(req, timeout=timeout)
+            code = handler.getcode()
+            response_body = handler
+        except HTTPError as e:
+            code = e.code
+            response_body = e
 
-    try:
-        logger.info("Normalizing response")
-        response_obj = MatchResponse.from_api(response_json)
-        response_api = response_obj.to_api()
-    except:
-        # log to console and return raw response
-        logger.error('Error normalizing response:\n{}'.format(e))
-        response_api = response_json
+        logger.info('Received HTTP {}'.format(code))
+        received_response_at = datetime.now()
+        elapsed_time = (received_response_at - sent_request_at).total_seconds()
+
+        logger.info("Loading response")
+        response_data = response_body.read().decode('utf-8')
+        response = json.loads(response_data)
+    except Exception as e:
+        logger.error('Request resulted in error: {}'.format(e))
+        return (code, { 'message': str(e) }, elapsed_time)
 
     # Inject server information
-    response_api['_server'] = {
-        'id': server.get('server_id'),
-        'baseUrl': server.get('base_url')
+    response['_server'] = {
+        'id': server_id,
+        'baseUrl': base_url,
     }
-    return response_api
 
-
-def proxy_request(request_data, timeout=5, server_ids=None):
-    from multiprocessing import Pool
-
-    db = get_backend()
-    all_servers = db.servers.list().get('rows', [])
-
-    servers = []
-    for server in all_servers:
-        if (server.get('direction') == 'out' and
-            (not server_ids or server['server_id'] in server_ids)):
-            servers.append(server)
-
-    handles = []
-    if servers:
-        pool = Pool(processes=4)
-
-        for server in servers:
-            handle = pool.apply_async(send_request, (server, request_data, timeout))
-            handles.append((server, handle))
-
-    results = []
-    for server, handle in handles:
-        try:
-            result = handle.get(timeout=timeout)
-        except TimeoutError:
-            print('Timed out')
-            result = None
-        except Exception as e:
-            print('Other error: {}'.format(e))
-            continue
-
-        results.append((server, result))
-    return results
+    return (code, response, elapsed_time)
 
 
 @app.route('/', methods=['GET'])
 @produces('text/html')
 def index():
-    servers = get_connected_servers()
-    return render_template('index.html', servers=servers)
+    servers = get_outgoing_servers()
+    recent_requests = get_recent_requests()
+    return render_template('index.html', servers=servers, recent_requests=recent_requests)
 
 
 @app.route('/v1/servers/<server_id>/match', methods=['POST'])
@@ -135,11 +184,12 @@ def match_server(server_id):
         response.headers['Content-Type'] = API_MIME_TYPE
         return response
 
-    timeout = int(request.args.get('timeout', 10))
+    timeout = int(flask_request.args.get('timeout', 10))
+    received_request_at = datetime.now()
 
     try:
         logger.info("Getting flask request data")
-        request_json = request.get_json(force=True)
+        request_json = flask_request.get_json(force=True)
     except BadRequest:
         error = jsonify(message='Invalid request JSON')
         error.status_code = 400
@@ -155,32 +205,49 @@ def match_server(server_id):
         return error
 
     logger.info("Parsing query")
-    request_obj = MatchRequest.from_api(request_json).to_api()
+    request = MatchRequest.from_api(request_json).to_api()
 
-    # Inject server data into request
-    request_server = g.get('server')
-    if request_server:
-        request_obj['_server'] = {
-            'server_id': request_server['server_id']
-        }
+    # Set server data in request
+    request['_server'] = {
+        'server_id': flask.g.get('server')['server_id']
+    }
 
     try:
-        validate_request(request_obj)
+        validate_request(request)
     except ValidationError as e:
-        error = jsonify(message='Created request does not conform to API specification')
-        print(request_obj)
+        error = jsonify(message='Normalized request does not conform to API specification')
         error.status_code = 422
         return error
 
-    logger.info("Preparing request to proxy")
-    request_data = json.dumps(request_obj).encode('utf-8')
+    logger.info("Proxying request to {}".format(server_id))
+    response_code, response_json, response_duration = send_request(server_id, request, timeout)
+    # Set error code for exchange server errors
+    if response_code == 0:
+        response_code = 500
 
-    logger.info("Proxying request")
-    logger.info(json.dumps(request_obj, indent=4))
-    server_responses = proxy_request(request_data, timeout=timeout, server_ids=[server_id])
-
-    server, response = server_responses[0]
     # Inject request data into response
-    response['request'] = request_obj
+    response_json['_request'] = request
 
-    return jsonify(response)
+    response = {}
+    if response_code == 200:
+        logger.info("Validating response syntax")
+        try:
+            validate_response(response_json)
+        except ValidationError as e:
+            # log to console and return response anyway
+            logger.warning('Response does not conform to API specification:\n{}'.format(e))
+
+        try:
+            logger.info("Normalizing response")
+            response = MatchResponse.from_api(response_json).to_api()
+        except:
+            # log to console and return raw response
+            logger.warning('Error normalizing response:\n{}'.format(e))
+
+    sender_id = flask.g.get('server')['server_id']
+    log_request(sender_id, request_json, request, server_id, response_code, response_json, response, received_request_at, response_duration)
+
+    # Pass through response code
+    result = jsonify(response)
+    result.status_code = response_code
+    return result
--- server/compat.py
+++ server/compat.py
@@ -11,3 +11,8 @@ try:
     from urllib2 import urlopen, Request
 except ImportError:
     from urllib.request import urlopen, Request
+
+try:
+    from urllib2 import HTTPError
+except ImportError:
+    from urllib.error import HTTPError
